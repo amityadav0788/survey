94d2593f57e76dc61ac960b1910422fee95fec11
Amit Yadav <amit.yadav@paytm.com>
2016-07-30 20:22:15 +0530

    Commiting googl vision api

--- api/visionGoogle.js
+++ api/visionGoogle.js
@@ -1,7 +1,21 @@
+var gcloud = require('gcloud')({
+  keyFilename: '/home/amit/Documents/Codebase/packages1/src/api/visonOauth.json',
+  projectId : 'cloudvisionapitest-1385'
+});
+
+var vision = gcloud.vision();
+
 var visionGoogle = {
 
+
   upload : function(params,cb){
-   cb(null,"hehehehe");
+    var image = '/home/amit/Documents/Codebase/packages1/src/api/image.png';
+
+   vision.detectText(image, function(err, text, apiResponse) {
+      // text = ['This was text found in the image']
+      console.log(err+" heheheh "+text+" ahehhehe "+JSON.stringify(apiResponse));
+    });
+   //cb(null,"hehehe");
   }
 };
 
